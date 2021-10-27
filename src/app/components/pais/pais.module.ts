a0441b86ef160d8cf81a592665a1aa188af78dd1
Débora Thomaz <deh.thomaz@gmail.com>
2021-10-27 20:21:57 -0300

    Primeiro commit projeto Angular

---
+++ src/app/components/pais/pais.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaisComponent } from './pais.component';
+import { PaisRoutingModule } from "./pais-routing.module";
+
+
+
+@NgModule({
+  declarations: [
+    PaisComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaisModule { }
---
+++ src/app/components/universidades/universidades.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UniversidadesComponent } from './universidades.component';
+import { UniversidadesRoutingModule } from "./universidade-routing.module";
+
+
+
+@NgModule({
+  declarations: [
+    UniversidadesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UniversidadesModule { }
