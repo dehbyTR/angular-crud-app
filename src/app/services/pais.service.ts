4a6c3f866a5976289170e5a8afb39d9e2f5a90ce
Débora Thomaz <deh.thomaz@gmail.com>
2021-11-01 19:45:52 -0300

    Classe PaisService completa

--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -8,4 +8,38 @@ import { environment } from "src/environments/environment";
 export class PaisService {
 
   constructor(private http: HttpClient) { }
+
+  /**
+   * função listar
+   */
+  public listar() {
+    return this.http.get(environment.url_api + 'paises');
+  }
+
+  /**
+   * criar
+   */
+  public criar(pais: any) {
+    return this.http.post(environment.url_api + 'paises', pais);
+  }
+
+  /**
+   * listarPorId
+   */
+  public listarPorId(id: number) {
+    return this.http.get(environment.url_api + 'paises/' + id);
+  }
+
+  /**
+   * atualizar
+   */
+  public atualizar(pais: any, id: number) {
+    return this.http.put(environment.url_api + 'paises/' + id, pais);
+  }
+
+  /**
+   * excluir */
+  public excluir(id: number) {
+    return this.http.delete(environment.url_api + 'paises/' + id);
+  }
 }
