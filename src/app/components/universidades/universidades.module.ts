5aad8090d22c16386e62e17490cd04d4db4ccbff
Débora Thomaz <deh.thomaz@gmail.com>
2021-11-17 20:09:59 -0300

    Adição dos serviços País e Universidades dentro do módulo Universidade

--- src/app/components/universidades/universidades.module.ts
+++ src/app/components/universidades/universidades.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UniversidadesComponent } from './universidades.component';
 import { UniversidadesRoutingModule } from "./universidade-routing.module";
+import { PaisService } from "src/app/services/pais.service";
+import { UniversidadesService } from "src/app/services/universidades.service";
 
 
 
@@ -10,7 +12,12 @@ import { UniversidadesRoutingModule } from "./universidade-routing.module";
     UniversidadesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UniversidadesRoutingModule
+  ],
+  providers: [
+    PaisService,
+    UniversidadesService
   ]
 })
 export class UniversidadesModule { }
