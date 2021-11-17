8697a5faaffb3bf87a1a98918e25ff0a6edcd136
Débora Thomaz <deh.thomaz@gmail.com>
2021-11-17 20:29:12 -0300

    Lógica do painel

--- src/app/components/painel/painel.component.ts
+++ src/app/components/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from "src/app/services/pais.service";
+import { UniversidadesService } from "src/app/services/universidades.service";
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public nuPaises: number = 0;
+  public nuUniversidades: number = 0;
+
+  constructor(
+    private _paisService: PaisService,
+    private _universidadesService: UniversidadesService
+  ) { }
 
   ngOnInit(): void {
+    this._paisService.listar().subscribe(resp => {
+      this.nuPaises = Object.values(resp).length;
+    }, err => {
+      console.error('Ocorreu um erro');
+    });
   }
 
 }
