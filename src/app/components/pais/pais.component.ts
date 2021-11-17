d7216bdb29997a27aec24a79b21142442f22a5e8
Débora Thomaz <deh.thomaz@gmail.com>
2021-11-17 20:59:56 -0300

    Lógica de País

--- src/app/components/pais/pais.component.ts
+++ src/app/components/pais/pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from "src/app/services/pais.service";
 
 @Component({
   selector: 'app-pais',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaisComponent implements OnInit {
 
-  constructor() { }
+  public paises: any = [];
+  public pais: any = this._iniciarPais();
+
+  constructor(
+    private _paisService: PaisService
+  ) { }
 
   ngOnInit(): void {
+    this.listar();
+  }
+
+  /**
+   * listar
+   */
+  public listar(): void {
+    this._paisService.listar().subscribe(resp => {
+      this.paises = resp;
+    });
+  }
+
+  /**
+   * salvar
+   */
+  public salvar(): void {
+    if (this.pais.nome !== "") {
+      this._paisService.criar(this.pais).subscribe(resp => {
+        alert('Cadastrado');
+        this.listar();
+        this.pais = this._iniciarPais();
+      });
+    }
+  }
+
+  /**
+   * editar
+   */
+  public editar(pais: any): void {
+    this.pais = Object.assign({}, pais);
+  }
+
+  /**
+   * atualizar
+   */
+  public atualizar(): void {
+    if (this.pais.nome !== "") {
+      this._paisService.atualizar(this.pais, this.pais.id).subscribe(resp => {
+        alert('Atualizado!');
+        this.listar();
+        this.pais = this._iniciarPais();
+      });
+    }
+  }
+
+  /**
+   * cancelar
+   */
+  public cancelar(): void {
+    this.pais = this._iniciarPais();
+  }
+
+  /**
+   * excluir
+   */
+  public excluir(id: number): void {
+    this._paisService.excluir(id).subscribe(resp => {
+      alert('Excluído!');
+      this.listar();
+    });
+  }
+  
+  private _iniciarPais() {
+    return {
+      id: null,
+      nome: ''
+    };
   }
 
 }
